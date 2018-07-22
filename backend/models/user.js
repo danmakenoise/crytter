04b0437b814d76343d78eb52855316437403999c
danmakenoise <danmakenoise@gmail.com>
2018-07-22 13:31:44 -0400

    feat(create database): generate user model

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    passwordHash: DataTypes.STRING,
+    passwordSalt: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};

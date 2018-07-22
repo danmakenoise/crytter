9e6082369c814563c84b8cfa1cb6c724438d29c4
danmakenoise <danmakenoise@gmail.com>
2018-07-22 17:33:46 -0400

    feat(add user controller): create user.create

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,27 +1,51 @@
+const bcrypt = require('bcrypt')
 const User = require('../models').User
+
+const validatePassword = password => {
+  const errors = []
+
+  if (!password) {
+    errors.push('password is required')
+  }
+
+  if (password && password.length < 8) {
+    errors.push('password must contain more than 8 charaters')
+  }
+
+  if (password && password.length > 72) {
+    errors.push('password must container fewer than 72 charaters')
+  }
+
+  return { errors }
+}
 
 module.exports = {
   create (req, res) {
+    const passwordValidation = validatePassword(req.body.password)
+
+    if (passwordValidation.errors.length) {
+      return res.status(400).send({ errors: passwordValidation.errors })
+    }
+
+    const passwordSalt = bcrypt.genSaltSync(10)
+    const passwordHash = bcrypt.hashSync(req.body.password, passwordSalt)
+
     return User.create({
       username: req.body.username,
-      passwordHash: 'passwordHash',
-      passwordSalt: 'passwordSalt'
+      passwordHash,
+      passwordSalt
     })
       .then(user => res.status(201).send({
         id: user.id,
         username: user.username
       }))
       .catch(error => {
-        if (error.name === 'SequelizeValidationError') {
-          res.status(400).send({
+        if (error.name.match(/Sequelize/)) {
+          return res.status(400).send({
             errors: error.errors.map(({ message }) => message)
           })
         } else {
-          res.status(400).send({
-            errors: [
-              'something went wrong'
-            ]
-          })
+          return res.status(500).send()
         }
       })
   }

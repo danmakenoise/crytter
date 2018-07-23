0f5b837f053be6c0ed68406515bb12bd39a50d54
danmakenoise <danmakenoise@gmail.com>
2018-07-22 23:27:56 -0400

    feat(base frontend): install linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 const App = () => (
-  <div className="App">
+  <div className='App'>
     <h1>Welcome to Crytter</h1>
   </div>
 )
 
-export default App;
+export default App

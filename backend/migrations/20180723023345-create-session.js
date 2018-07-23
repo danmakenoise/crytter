bd0901f24f4bbc9f214c89a9bae3fe337ab2394e
danmakenoise <danmakenoise@gmail.com>
2018-07-22 23:01:02 -0400

    feat(implement sessions): add base id to session

--- backend/migrations/20180723023345-create-session.js
+++ backend/migrations/20180723023345-create-session.js
@@ -2,6 +2,11 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Sessions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
       sid: {
         allowNull: false,
         type: Sequelize.STRING(32),

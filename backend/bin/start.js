fd48e94e817b3f802822b98fc786570cf51425c5
danmakenoise <danmakenoise@gmail.com>
2018-07-22 21:45:11 -0400

    feat(deploy backend to heroku): authenticate database before launching server

--- backend/bin/start.js
+++ backend/bin/start.js
@@ -1,7 +1,10 @@
 const app = require('../app')
+const db = require('../models')
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-  console.log(`server listening on localhost:${PORT}`)
+db.sequelize.authenticate().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server listening on localhost:${PORT}`)
+  })
 })

5534bb23e51905ad3cc1030fc05b38072f330989
danmakenoise <danmakenoise@gmail.com>
2018-07-23 01:00:36 -0400

    feat(add signup on frontend): form sends post request for signup

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactRouterPropTypes from 'react-router-prop-types'
+import { withRouter } from 'react-router-dom'
+import { signupUser } from '../services/user'
 
-const Signup = () => (
-  <h1>Signup</h1>
-)
+const propTypes = {
+  history: ReactRouterPropTypes.history.isRequired
+}
 
-export default Signup
+class Signup extends Component {
+  constructor (props) {
+    super(props)
+
+    this.$username = React.createRef()
+    this.$password = React.createRef()
+    this.$passwordConfirm = React.createRef()
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+    this.state = {
+      errors: []
+    }
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+
+    const username = this.$username.current.value
+    const password = this.$password.current.value
+    const passwordConfirm = this.$passwordConfirm.current.value
+
+    signupUser({ username, password, passwordConfirm })
+      .then((errors) => {
+        if (errors) {
+          this.setState({ errors })
+          return
+        }
+
+        this.props.history.push('/login')
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Signup</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='signupUsername'>Username:
+            <input type='text' name='signupUsername' ref={this.$username} />
+          </label>
+
+          <label htmlFor='signupPassword'>Password:
+            <input type='password' name='signupPassword' ref={this.$password} />
+          </label>
+
+          <label htmlFor='signupConfirmPassword'>Confirm Password:
+            <input type='password' name='signupConfirmPassword' ref={this.$passwordConfirm} />
+          </label>
+
+          <button>Signup</button>
+        </form>
+
+        {!!this.state.errors.length && (
+          <div>
+            <h3>Errors:</h3>
+
+            <ul>
+              {this.state.errors.map((error, index) => (
+                <li key={index}>{error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+Signup.propTypes = propTypes
+export default withRouter(Signup)
---
+++ frontend/src/services/user.js
@@ -0,0 +1,59 @@
+const config = {
+  development: {
+    api: 'http://localhost:3001'
+  },
+  production: {
+    api: 'https://crytter-backend.herokuapp.com'
+  }
+}[process.env.NODE_ENV]
+
+const validateParams = (params) => {
+  const errors = []
+
+  if (!params.username || !params.password || !params.passwordConfirm) {
+    errors.push('Username, Password, and Password Confirmation are required.')
+    return errors
+  }
+
+  if (params.username.length < 3) {
+    errors.push('Username must be longer than 3 characters.')
+  } else if (params.username.length > 15) {
+    errors.push('Username must be shorter than 15 character.')
+  }
+
+  if (params.password.length < 8) {
+    errors.push('Password must be longer than 3 characters.')
+  } else if (params.password.length > 74) {
+    errors.push('Username must be shorter than 74 character.')
+  }
+
+  if (params.password !== params.passwordConfirm) {
+    errors.push('Passwords must match.')
+  }
+
+  return errors
+}
+
+export const signupUser = (params) => new Promise((resolve, reject) => {
+  const errors = validateParams(params)
+
+  if (errors.length) {
+    resolve(errors)
+    return
+  }
+
+  window.fetch(
+    `${config.api}/users`,
+    {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: params.username,
+        password: params.password
+      })
+    }
+  ).then(resolve)
+})

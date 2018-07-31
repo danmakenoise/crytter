2a7396d9f12eefb3a78d87a7133988ac111c9e4c
danmakenoise <danmakenoise@gmail.com>
2018-07-30 23:40:43 -0400

    feat(material ui setup): move username to app bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -12,6 +12,7 @@ import MenuItem from '@material-ui/core/MenuItem'
 import MenuIcon from '@material-ui/icons/Menu'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 
 const propTypes = {
   currentMenuAnchor: PropTypes.node.isRequire,
@@ -20,9 +21,20 @@ const propTypes = {
   history: ReactRouterPropTypes.history.isRequired,
   isLoggedIn: PropTypes.bool.isRequired,
   isMenuOpen: PropTypes.bool.isRequired,
+  myUsername: PropTypes.string.isRequired,
   navigateToLogin: PropTypes.func.isRequired,
   navigateToLogout: PropTypes.func.isRequired,
   navigateToSignup: PropTypes.func.isRequired
+}
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  appTitle: {
+    flexGrow: 1,
+    marginLeft: 20
+  },
 }
 
 const enhance = compose(
@@ -53,37 +65,46 @@ const enhance = compose(
     navigateToLogin: props => () => props.closeMenuAndNavigate('/login'),
     navigateToLogout: props => () => props.closeMenuAndNavigate('/logout'),
     navigateToSignup: props => () => props.closeMenuAndNavigate('/signup')
-  })
+  }),
+  withStyles(styles)
 )
 
 const NavBar = props => (
-  <AppBar position='static' color='default'>
-    <Toolbar>
-      <IconButton color='inherit' onClick={props.handleOpenMenu}>
-        <MenuIcon />
-      </IconButton>
-      <Menu
-        anchorEl={props.currentMenuAnchor}
-        open={props.isMenuOpen}
-        onClose={props.handleCloseMenu}
-      >
-        {props.isLoggedIn &&
-          <React.Fragment>
-            <MenuItem onClick={props.navigateToLogout}>Logout</MenuItem>
-          </React.Fragment>
-        }
-        {!props.isLoggedIn &&
-          <React.Fragment>
-            <MenuItem onClick={props.navigateToLogin}>Login</MenuItem>
-            <MenuItem onClick={props.navigateToSignup}>Signup</MenuItem>
-          </React.Fragment>
-        }
-      </Menu>
-      <Typography variant='title' color='inherit'>
-        Crytter
-      </Typography>
-    </Toolbar>
-  </AppBar>
+  <div styles={styles.root}>
+    <AppBar position='static' color='default'>
+      <Toolbar>
+        <IconButton color='inherit' onClick={props.handleOpenMenu}>
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          anchorEl={props.currentMenuAnchor}
+          open={props.isMenuOpen}
+          onClose={props.handleCloseMenu}
+        >
+          {props.isLoggedIn &&
+            <React.Fragment>
+              <MenuItem onClick={props.navigateToLogout}>Logout</MenuItem>
+            </React.Fragment>
+          }
+          {!props.isLoggedIn &&
+            <React.Fragment>
+              <MenuItem onClick={props.navigateToLogin}>Login</MenuItem>
+              <MenuItem onClick={props.navigateToSignup}>Signup</MenuItem>
+            </React.Fragment>
+          }
+        </Menu>
+        <Typography variant='title' color='inherit' style={styles.appTitle}>
+          Crytter
+        </Typography>
+        <Typography variant='subheading' color='inherit' style={styles.flexGrow}>
+          {props.isLoggedIn
+            ? <span>Signed In As: <em>{props.myUsername}</em></span>
+            : <span>Signed Out</span>
+          }
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
 )
 
 NavBar.propTypes = propTypes
